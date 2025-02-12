de7e28ee8ca40af63815d3b6dd704cf096f3acb7
kirillkychkin <kychkin.kirill@gmail.com>
2025-02-12 10:49:31 +0900

    add menu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            выходToolStripMenuItem = new ToolStripMenuItem();
+            выходИзПрограммыToolStripMenuItem = new ToolStripMenuItem();
+            справкаToolStripMenuItem = new ToolStripMenuItem();
+            оПрограммеToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { выходToolStripMenuItem, справкаToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // выходToolStripMenuItem
+            // 
+            выходToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { выходИзПрограммыToolStripMenuItem });
+            выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            выходToolStripMenuItem.Size = new Size(54, 20);
+            выходToolStripMenuItem.Text = "Выход";
+            // 
+            // выходИзПрограммыToolStripMenuItem
+            // 
+            выходИзПрограммыToolStripMenuItem.Name = "выходИзПрограммыToolStripMenuItem";
+            выходИзПрограммыToolStripMenuItem.Size = new Size(193, 22);
+            выходИзПрограммыToolStripMenuItem.Text = "Выход из программы";
+            выходИзПрограммыToolStripMenuItem.Click += выходИзПрограммыToolStripMenuItem_Click;
+            // 
+            // справкаToolStripMenuItem
+            // 
+            справкаToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { оПрограммеToolStripMenuItem });
+            справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            справкаToolStripMenuItem.Size = new Size(65, 20);
+            справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            оПрограммеToolStripMenuItem.Size = new Size(180, 22);
+            оПрограммеToolStripMenuItem.Text = "О программе";
+            оПрограммеToolStripMenuItem.Click += оПрограммеToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem выходToolStripMenuItem;
+        private ToolStripMenuItem выходИзПрограммыToolStripMenuItem;
+        private ToolStripMenuItem справкаToolStripMenuItem;
+        private ToolStripMenuItem оПрограммеToolStripMenuItem;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,19 @@ namespace lab2
         {
             InitializeComponent();
         }
+        private string getAboutText()
+        {
+            return "ѕрограмма разработана в 2025 году в рамках каурса 'языки программировани€ и методы трансл€ции' в качестве второй лабораторной работы." + Environment.NewLine + Environment.NewLine + "ѕрограмма создана дл€ обработки символьных строк";
+        }
+
+        private void оѕрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(getAboutText());
+        }
+
+        private void выход»зѕрограммыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
