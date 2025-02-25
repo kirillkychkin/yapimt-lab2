0ebe5ff8d0d94f1ef667120684a394573380a1f9
Kirill Kychkin <kychkin.ka@albank.ru>
2025-02-25 20:10:27 +0900

    add test input method inside specific class

---
+++ TestInput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    static class TestInput
+    {
+        public static int TestInt32(string input)
+        {
+
+            if (input.Length == 0)
+            {
+                MessageBox.Show("Строка не содержит число!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+
+            try
+            {
+                int number = Int32.Parse(input);
+                return number;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Невозможно преобразовать строку в число: неверный формат", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Ошибка переполнения", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return 0;
+        }
+    }
+}
